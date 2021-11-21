b2f373d2b606d06fa9de20eef16dff49eaef00b6
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-21 18:30:10 -0300

    Refatorando o componente item de lista com funcoes in-line

--- src/Question(version with namedFns).js
+++ src/Question(version with namedFns).js
@@ -8,14 +8,28 @@ const Question = ({ title, info }) => {
   const [ showInfo, setShowInfo ] = useState(false);
 
 
+  const renderIcon = () => {
+    if (showInfo) {
+      return <AiOutlineMinus />;
+    } else {
+      return <AiOutlinePlus />
+    }
+  };
+  
+  const toggleBtn = () => {
+		if (!showInfo) {
+		  setShowInfo(true);
+		} else {
+			setShowInfo(false);
+		}
+	};
+
 
   return (
     <article className="question">
       <header>
 			  <h4>{title}</h4>
-        <button className="btn" onClick={() => setShowInfo(!showInfo)} >
-          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
-        </button>
+        <button className="btn" onClick={toggleBtn} >{renderIcon()}</button>
       </header>
 
       {showInfo && <p>{info}</p>}
