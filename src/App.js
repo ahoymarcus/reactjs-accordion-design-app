f44ba016698b0ef632131bac3caf67af643160ea
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-21 17:48:00 -0300

    Definir estrutura Jsx basica de App.js. Renderizar componente filho de item lista passando os dados do arquivo de origem.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React, { useState } from 'react';
+
 import data from './data';
 import SingleQuestion from './Question';
+
+
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  // console.log(data);
+
+  const renderQuestions = data.map((question) => {
+		
+		return <SingleQuestion key={question.id} {...question} />;
+	});
+
+  return (
+    <main>
+			<header>
+				<h1>Questions</h1>
+			</header>
+			<div>
+				<ul>
+					{renderQuestions}
+				</ul>
+			</div>
+		</main>
+  );
 }
 
+
+
+
 export default App;
+
+
