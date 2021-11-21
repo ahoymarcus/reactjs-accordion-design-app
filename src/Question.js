bf01b9a451a0b343c6aa4aac5f604207dd6b84d0
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-21 18:00:06 -0300

    Definir estrutura basica do componente item de lista e apresentar primeiros recebidos por props

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
+
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+
+
+const Question = ({ id, title, info }) => {
+  
+  // const toggleBtn = () ==> {
+	// 	if () {
+	// 		return <button onClick={} >+</button>;
+	// 	} else {
+	// 		return <button onClick={} >-</button>;
+	// 	}
+	// };
+
+
+
+  return (
+    <div>
+			<h4>{title}</h4>
+			<p>{info}</p>
+		</div>
+		
+    /*
+		  {toggleBtn}
+    */
+  );
 };
 
+
+
+
 export default Question;
+
+
+
